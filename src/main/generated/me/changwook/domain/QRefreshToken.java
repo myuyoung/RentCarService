dc6ed4c50473856b6dab4bb210ff06fb195b47bb
myuyoung <parkcw5784@gmail.com>
2025-05-12 21:53:51 +0900

    2025-05-12

---
+++ src/main/generated/me/changwook/domain/QRefreshToken.java
@@ -0,0 +1,43 @@
+package me.changwook.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QRefreshToken is a Querydsl query type for RefreshToken
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QRefreshToken extends EntityPathBase<RefreshToken> {
+
+    private static final long serialVersionUID = -1868124883L;
+
+    public static final QRefreshToken refreshToken = new QRefreshToken("refreshToken");
+
+    public final NumberPath<Long> expiryDate = createNumber("expiryDate", Long.class);
+
+    public final StringPath id = createString("id");
+
+    public final StringPath token = createString("token");
+
+    public final StringPath username = createString("username");
+
+    public QRefreshToken(String variable) {
+        super(RefreshToken.class, forVariable(variable));
+    }
+
+    public QRefreshToken(Path<? extends RefreshToken> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QRefreshToken(PathMetadata metadata) {
+        super(RefreshToken.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/me/changwook/mapper/impl/RentMapperImpl.java
@@ -0,0 +1,109 @@
+package me.changwook.mapper.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import me.changwook.DTO.RentCarsDTO;
+import me.changwook.DTO.RentDTO;
+import me.changwook.domain.Rent;
+import me.changwook.domain.RentCars;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-08T16:24:27+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Amazon.com Inc.)"
+)
+@Component
+public class RentMapperImpl implements RentMapper {
+
+    @Override
+    public RentDTO rentToRentDTO(Rent rent) {
+        if ( rent == null ) {
+            return null;
+        }
+
+        RentDTO.RentDTOBuilder rentDTO = RentDTO.builder();
+
+        rentDTO.rentDate( rent.getRentDate() );
+        rentDTO.duration( rent.getDuration() );
+        rentDTO.endDate( rent.getEndDate() );
+        rentDTO.rentCars( rentCarsToRentCarsDTO( rent.getRentCars() ) );
+
+        return rentDTO.build();
+    }
+
+    @Override
+    public List<RentDTO> rentDTOListToRentDTOs(List<Rent> rents) {
+        if ( rents == null ) {
+            return null;
+        }
+
+        List<RentDTO> list = new ArrayList<RentDTO>( rents.size() );
+        for ( Rent rent : rents ) {
+            list.add( rentToRentDTO( rent ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Rent rentDTOToRent(RentDTO rentDTO) {
+        if ( rentDTO == null ) {
+            return null;
+        }
+
+        Rent.RentBuilder rent = Rent.builder();
+
+        rent.rentDate( rentDTO.getRentDate() );
+        rent.duration( rentDTO.getDuration() );
+        rent.endDate( rentDTO.getEndDate() );
+        rent.rentCars( rentCarsDTOToRentCars( rentDTO.getRentCars() ) );
+
+        return rent.build();
+    }
+
+    @Override
+    public List<Rent> rentListToRents(List<RentDTO> rentDTOList) {
+        if ( rentDTOList == null ) {
+            return null;
+        }
+
+        List<Rent> list = new ArrayList<Rent>( rentDTOList.size() );
+        for ( RentDTO rentDTO : rentDTOList ) {
+            list.add( rentDTOToRent( rentDTO ) );
+        }
+
+        return list;
+    }
+
+    protected RentCarsDTO rentCarsToRentCarsDTO(RentCars rentCars) {
+        if ( rentCars == null ) {
+            return null;
+        }
+
+        RentCarsDTO.RentCarsDTOBuilder rentCarsDTO = RentCarsDTO.builder();
+
+        rentCarsDTO.name( rentCars.getName() );
+        rentCarsDTO.rentPrice( rentCars.getRentPrice() );
+        rentCarsDTO.recommend( rentCars.getRecommend() );
+        rentCarsDTO.rentCarNumber( rentCars.getRentCarNumber() );
+
+        return rentCarsDTO.build();
+    }
+
+    protected RentCars rentCarsDTOToRentCars(RentCarsDTO rentCarsDTO) {
+        if ( rentCarsDTO == null ) {
+            return null;
+        }
+
+        RentCars.RentCarsBuilder rentCars = RentCars.builder();
+
+        rentCars.rentCarNumber( rentCarsDTO.getRentCarNumber() );
+        rentCars.name( rentCarsDTO.getName() );
+        rentCars.recommend( rentCarsDTO.getRecommend() );
+        rentCars.rentPrice( rentCarsDTO.getRentPrice() );
+
+        return rentCars.build();
+    }
+}
