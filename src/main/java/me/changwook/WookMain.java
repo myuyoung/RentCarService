1449eba009533ad8d9e7fe18cef2b13c98a1dddb
myuyoung <parkcw5784@gmail.com>
2025-05-31 08:06:44 +0900

    2025-05-31

--- src/main/java/me/changwook/WookMain.java
+++ src/main/java/me/changwook/WookMain.java
@@ -2,12 +2,25 @@ package me.changwook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 public class WookMain {
+
     public static void main(String[] args) {
         SpringApplication.run(WookMain.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider(){
+        return () -> Optional.of(UUID.randomUUID().toString());
     }
 }
