b6a7a1e6bfee723076e266a6cb0def8716f27051
myuyoung <parkcw5784@gmail.com>
2025-07-02 10:36:50 +0900

    2025-07-02

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,6 +5,11 @@ APP_DIR="/home/ec2-user/app"
 JAR_NAME=$(ls -tr $APP_DIR/*.jar | tail -n 1)
 LOG_FILE="$APP_DIR/deploy.log"
 
+export Spring_Mail_UserName="jjjonga33@naver.com"
+export Spring_Mail_Password="WX1QPXDJ87N7"
+export Jwt_Secret="qwertyuiopasdfghjklzxcvbnmqwerty"
+export Admin_Email="parkcw5784@gmail.com"
+
 echo "> 새 애플리케이션을 배포합니다: $JAR_NAME"
 # nohup: 터미널 세션이 끊겨도 프로세스가 계속 실행되도록 함
 # 2>&1: 표준 에러를 표준 출력으로 리다이렉션
